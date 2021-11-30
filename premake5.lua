1584fe1f7555a2ef77baa8407f99bc2be01719e8
Exelcior <Exelcior2012@users.noreply.github.com>
2021-11-30 23:58:01 +0000

    Set warnings to W4/High, disable 4100 (unref param)

--- premake5.lua
+++ premake5.lua
@@ -14,7 +14,12 @@ project "AdventOfCode2021"
 
 	rtti "Off"
 	exceptionhandling "Off"
-	warnings "Everything"
+	warnings "High"
+	
+	disablewarnings { 
+		"4100" -- Unreferenced formal parameter, no real downside
+	}
+	
 	flags {"FatalWarnings"}
 	conformancemode "On"
 
@@ -22,6 +27,10 @@ project "AdventOfCode2021"
 		"Include/**.h",
 		"Source/**.h",
 		"Source/**.cpp"
+	}
+	
+	includedirs {
+		"Include"
 	}
 
 	-- Reproduce the file structure inside the solution explorer in visual studio
